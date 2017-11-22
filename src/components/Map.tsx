6ad3dfe960c4872ec7f4ecbde34724d623285172
Tim Myers <tmyers@closetbox.me>
2017-11-22 14:57:28 -0700

    map markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
 import GoogleMap from 'google-map-react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 const defaultProps = {
   center: { lat: 39.710206, lng: -104.990482 },
   zoom: 11,
 };
 
-const Map = () => (
+const Marker = (props: any) => (
+  <img src={props.src} width={40} height={40}/>
+  );
+
+const Map = (props: any) => (
   <GoogleMap
     bootstrapURLKeys={{
       key: 'AIzaSyDk64oknr1zOjz-loIogxns15U1ZWV5luc',
     }}
     defaultCenter={defaultProps.center}
     defaultZoom={defaultProps.zoom}
-  />
+  >
+    {!props.data.loading && props.data.allBreweries.map((brewery: any) => (
+      <Marker
+        lat={brewery.lat}
+        lng={brewery.lng}
+        src="https://image.flaticon.com/icons/svg/33/33622.svg"
+      />
+    ))}
+  </GoogleMap>
 );
 
-export default Map;
+const MapWithData = graphql(gql`
+  query {
+    allBreweries {
+      lat, lng, id
+    }
+  }
+`)(Map);
+
+export default MapWithData;
--- src/layouts/BigScreen.tsx
+++ src/layouts/BigScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+// import { Route, Router } from 'react-router-dom';
 import styled from 'styled-components';
 import Map from 'Components/Map';
 import BreweryList from 'Components/BreweryList';
