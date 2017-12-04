968c34d7e2375ad69e33f4df89eb2dcd82469d62
Tim Myers <tmyers@closetbox.me>
2017-12-03 19:40:07 -0700

    go to new api

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -3,6 +3,6 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9ke7cbc0ode01978ivka908' }),
+  link: new HttpLink({ uri: 'https://api.brewmap.co/graphql' }),
   cache: new InMemoryCache(),
 });
