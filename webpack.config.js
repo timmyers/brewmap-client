4876af73a340491621b09dd786200f8da7604a2f
Tim Myers <tmyers@closetbox.me>
2017-12-14 16:27:24 -0700

    heap anaylytics

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ const HtmlWebpackTemplate = require('html-webpack-template');
 const DashboardPlugin = require('webpack-dashboard/plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const heapSnippet = '<script type="text/javascript">
+  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+    heap.load("3578566086");
+</script>';
+
 module.exports = {
   entry: './src/index.tsx',
   output: {
@@ -41,7 +46,8 @@ module.exports = {
       template: HtmlWebpackTemplate,
       appMountId: 'root',
       title: 'CO BrewMap',
-      favicon: './src/images/favicon.ico'
+      favicon: './src/images/favicon.ico',
+      headHtmlSnippet: heapSnippet,
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
