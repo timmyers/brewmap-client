077d08e3e0f3c45905d98be9fca6318a647b25e6
Tim Myers <tmyers@closetbox.me>
2018-01-03 19:07:46 -0700

    source map

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  // devtool: 'eval-source-map',
+  devtool: 'source-map',
   module: {
     rules: [
       {
