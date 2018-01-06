5434887d1be191d45f8788f6cb50869e609b31ff
Tim Myers <tmyers@closetbox.me>
2018-01-05 17:24:48 -0700

    Fix stupid condition change

--- src/state/Brewery.tsx
+++ src/state/Brewery.tsx
@@ -19,11 +19,10 @@ class BreweryState {
   @computed get breweriesInView() {
     return this.sortedBreweries
       .filter((brewery: any) => {
-        if (brewery.lat > MapStore.viewbox.top) return false;
-        if (brewery.lat < MapStore.viewbox.bottom) return false;
-        if (brewery.lng > MapStore.viewbox.right) return false;
-        if (brewery.lng < MapStore.viewbox.left) return false;
-        return true;
+        return brewery.lat < MapStore.viewbox.top &&
+               brewery.lat > MapStore.viewbox.bottom &&
+               brewery.lng < MapStore.viewbox.right &&
+               brewery.lng > MapStore.viewbox.left;
       });
   }
 }
