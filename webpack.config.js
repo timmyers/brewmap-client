326e9b92a594ab5d064b2246afa5322a7abb3269
Tim Myers <tmyers@closetbox.me>
2018-01-24 20:19:25 -0700

    remvoe

--- webpack.config.js
+++ webpack.config.js
@@ -38,27 +38,6 @@ drift.SNIPPET_VERSION = "0.3.1";
 drift.load("74628t7nfckm");
 </script>
 <!-- End of Async Drift Code -->`;
-headSnippet += `<!-- Facebook Pixel Code -->
-  < script >
-  !function (f, b, e, v, n, t, s) {
-    if (f.fbq) return; n = f.fbq = function () {
-      n.callMethod ?
-      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    };
-    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = "2.0";
-    n.queue = []; t = b.createElement(e); t.async = !0;
-    t.src = v; s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s)
-  }(window, document, "script",
-    "https://connect.facebook.net/en_US/fbevents.js");
-fbq("init", "529920027385445");
-fbq("track", "PageView");
-</script >
-  <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=529920027385445&ev=PageView&noscript=1"
-  /></noscript>
-  <!--End Facebook Pixel Code-- >
-`;
 headSnippet += '<meta name="application-name" content="Brewed Here">';
 headSnippet += '<meta name="description" content="Find, discover, and track your visits to Colorado breweries.">';
 
